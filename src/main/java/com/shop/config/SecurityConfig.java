7fd222a433f80c9d081171a92b371406835d8e19
tinoKim <gksmfwlsgh@nate.com>
2022-03-21 21:04:49 +0900

    security-fix

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -43,4 +43,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(memberService).passwordEncoder((passwordEncoder()));
 
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher(
+                        ("/members/logout")))
+                .logoutSuccessUrl("/")
+        ;
+        http.authorizeRequests()
+                .mvcMatchers("/", "/members/**",
+                        "item/**", "/images/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        ;
+
+        http.exceptionHandling()
+                .authenticationEntryPoint(
+                        new CustomAuthenticationEntryPoint()
+                )
+        ;
+    }
 }
