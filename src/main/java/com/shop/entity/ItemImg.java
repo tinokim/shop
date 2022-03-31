e24c6f0811b629e486423176f8b7da762e6e36b1
tinoKim <gksmfwlsgh@nate.com>
2022-03-31 22:44:07 +0900

    Entity + fix

--- src/main/java/com/shop/entity/ItemImg.java
+++ src/main/java/com/shop/entity/ItemImg.java
@@ -1,4 +1,35 @@
 package com.shop.entity;
 
-public class ItemImg {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_img")
+@Getter
+@Setter
+public class ItemImg extends BaseEntity {
+    @Id
+    @Column(name = "item_img_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String imgName; // 이미지 파일명
+
+    private String oriImgName; // 원본 이미지 파일명
+
+    private String imgUrl; // 이미지 경로
+
+    private String repimgYn; // 대표 이미지 유무
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    public void updateItemImg(String oriImgName, String imgName, String ingUrl){
+        this.oriImgName = oriImgName;
+        this.imgName = imgName;
+        this.imgUrl = ingUrl;
+    }
 }
